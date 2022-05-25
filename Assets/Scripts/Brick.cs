01e0e41c3c431c3905e370c2dc594caefc57b2d7
AnakinSkyR4zor <TheLightShadow@protonmail.com>
2022-05-25 17:47:41 +0100

    Add source code

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] states;
+    public int health { get; private set; }
+    public int points = 100;
+    public bool unbreakable;
+
+    private void Awake() {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        ResetBrick();
+    }
+
+    public void ResetBrick() {
+        if (!this.unbreakable) {
+            this.health = this.states.Length;
+            this.spriteRenderer.sprite = this.states[this.health - 1];
+        }
+    }
+
+    private void Hit() {
+        if (this.unbreakable) {
+            return;
+        }
+
+        this.health--;
+
+        if (this.health <= 0) {
+            this.gameObject.SetActive(false);
+        } else {
+            this.spriteRenderer.sprite = this.states[this.health - 1];
+        }
+
+        FindObjectOfType<GameManager>().Hit(this);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Ball") {
+            Hit();
+        }
+    }
+
+}
---
+++ Assets/Scripts/MissZone.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MissZone : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Ball") {
+            FindObjectOfType<GameManager>().Miss();
+        }
+    }
+}
